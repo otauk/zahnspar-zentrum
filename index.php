5e996c62925d53c3ad682a0d05e12d641fc2107a
KUATOproject <kuato@gmx.net>
2014-12-17 15:41:35 +0100

    Erfahrungsberichte ergänzt / Slider-Navi

--- index.php
+++ index.php
@@ -60,6 +60,56 @@ Jede Art von Zahnersatz ist so individuell wie der Patient der ihn trägt. Die B
 			       </p>
 		        </div>
 			</div>
+			<div class="row">
+				<div class="col-6">
+					<div class="slide_container">
+					<h1>Erfahrungsberichte</h1>
+					<div class="slide_nav">
+						<nav>
+							<a class="nav_btn">
+								<input  type="radio" name="slidenav" checked="checked">
+							</a>
+							<a class="nav_btn">
+								<input  type="radio" name="slidenav">
+							</a>
+						</nav>
+
+					</div>
+
+						<div class="slide_content">
+							<div class="cont_wrap">
+								<div class="cont_slice">
+									<h2>Ein zufriedenes Lächeln</h2>
+									<p>
+					Holger S. (23) hatte sich nach langem Zögern doch entschlossen, mit seinen immer wiederkehrenden Zahnschmerzen zum Arzt zu gehen. Die Ursache für den Schmerz war zwar schnell gefunden, allerdings waren zur endgültigen Beseitigung leider einige Maßnahmen erforderlich.
+									</p>
+									<p>
+					Als der Zahnarzt Holger über den Befund aufklärte, wurde ihm schnell klar: Das wird teuer. Holger hatte vor einigen Jahren schon mal Zahnersatz erhalten und konnte damals, da er sich noch in der Ausbildung befand, die Zuzahlung nur durch einen Ratenkredit begleichen. Da er aber gerade am Anfang seines Berufslebens stand und er zudem bald Vater werden würde, war jeder gesparte Euro eine Investition in die Zukunft. "An der Gesundheit spart man nicht“ hatte ihm seine Mutter immer gesagt. Was aber, wenn es eine Möglichkeit gäbe, nicht an der Gesundheit, sondern an den damit verbundenen Kosten zu sparen? Gleichbleibende Qualität bei gleichzeitig günstigerem Preis, das wäre nach Holgers Geschmack.
+									</p>
+									<p>
+					Da fiel ihm ein, dass er beim Einkaufen in Hamm die Werbung für das <span class="brand">ZAHNSPAR-ZENTRUM</span> gesehen hatte. Nach einer schnellen Recherche im Netz war die Telefonnummer gefunden und nach einem kurzen Gespräch wurde direkt ein Termin vereinbart. Holger musste lediglich seinen Heil- und Kostenplan mitbringen, alles andere erledigte das Team vom <span class="brand">ZAHNSPAR-ZENTRUM</span>. Er wurde umfassend über die Einsparmöglichkeiten bei seinem Zahnersatz beraten und zu guter Letzt stellte sich heraus, dass nicht nur diese Beratung, sondern der gesamte Prozess vom ersten Gespräch bis zum fertigen Zahnersatz komplett kostenlos war.
+									</p>
+									<p>
+					Holger nahm das Angebot an, der Zahnersatz wurde professionell angefertigt und nach wenigen Tagen konnte Holger die Zahnarztpraxis mit einem zufriedenen Lächeln verlassen: Der Schmerz war endlich weg und er hatte obendrein noch 734,21 € gespart.
+									</p>
+								</div>
+								<div class="cont_slice">
+									<h2>Rentnerin spart 900€ beim Zahnersatz</h2>
+									<p>
+Zu Beginn des Jahres eröffnete das erste <span class="brand">ZAHNSPAR-ZENTRUM</span> in der Bahnhofstraße in Hamm. Es wird täglich von Patienten besucht, die den kostenfreien Beratungsservice für Zahnersatz in Anspruch nehmen. Anhand des Heil- und Kostenplans, den die Patienten von ihrem Zahnarzt erhalten, wird dann ein Vergleichsangebot erstellt. In allen Fällen konnten die Patienten bei ihrem Eigenanteil viel Geld sparen – manche sogar bis zu 80%.
+									</p>
+									<p>
+So erging es auch der Rentnerin Gerlind H. aus Hamm. „Meine Brücke besitze ich seit nun mehr als 20 Jahren. Nach erneutem Zahnverlust musste diese nun zwangsläufig erneuert werden. Mein Zahnarzt machte mir einen Kostenvoranschlag, der mir erst einmal den Atem raubte. Anders als früher sollte ich einen Eigenanteil dazu bezahlen – insgesamt 1296€“, so die Rentnerin, „Bei meiner kleinen Rente sind solche Anschaffungen einfach nicht möglich.“
+									</p>
+									<p>
+Aus der Zeitung erfuhr Gerlind H. von dem Beratungszentrum. Nach einer persönlichen und unverbindlichen Beratung bekam sie ein Vergleichsangebot und bei einem verbleibenden Eigenanteil von 378€ konnte sie Ihr Glück kaum fassen. Nächsten Monat erhält sie ihre neue Brücke zum „Sparpreis“ und sagt glücklich: “Für mich hat sich der Weg ins <span class="brand">ZAHNSPAR-ZENTRUM</span> mehr als gelohnt.“
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="container">
 			<div class="row">
@@ -92,4 +142,13 @@ Jede Art von Zahnersatz ist so individuell wie der Patient der ihn trägt. Die B
 		</div>
 		</div>
 	</body>
+	<script>
+		$(document).ready(function() {
+			$('nav').on('click', 'a', function(){
+				$(".slide_content").css("transform","translateX("+$(this).index() * -100+"%)");
+				$("nav a div").removeClass('current');
+				$(this).find('div').addClass('current');
+			});
+		});
+	</script>
 </html>

cea4b65ce3a036d69a16b5b7ead4b5ce83ec8cd6
KUATOproject <kuato@gmx.net>
2014-12-17 16:53:38 +0100

    minor changes

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<title>Zahnspar-Zentrum</title>
 		<meta charset="utf-8">
 		<meta http-equiv="expires" content="0">
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 		<!-- Font -->
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>

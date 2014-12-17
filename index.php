3cb8a7db0fe9a6c1ec7e4904961a08ce4701e344
KUATOproject <kuato@gmx.net>
2014-12-17 03:36:10 +0100

    Redesign  // steht bis auf footer

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>KUATOplate</title>
+		<!-- Font -->
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<!-- Stylesheet -->
+		<link href="css/custom-base.css" rel="stylesheet" type="text/css"/>
+		<!-- jQuery -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			 <div class="row">
+				<div class="col-6">
+					<div class="header"></div>
+				</div>
+			 </div>
+			 <div class="row">
+				<div class="col-6">
+					<div class="alert danger">Sparen Sie bis zu 80% Ihrer Zahnersatzkosten</div>
+				</div>
+			 </div>
+			<div class="row">
+		        <div class="col-3">
+			        <h1>Interviews mit unseren Kunden</h1>
+					<p>
+						<iframe width="100%" height="360" src="//www.youtube.com/embed/p3lgDrKQMF0?list=RDEwQTmjK6zo0" frameborder="0" allowfullscreen></iframe>
+					</p>
+					<h2>Kunden erzählen ihre Geschichte</h2>
+						Schauen Sie sich die Videos an und hören Sie von unseren Kunden selbst, wie zufrieden Sie mit dem Zahnspar-Zentrum sind. Erfahren Sie, wieso eine Rentnerin trotz hoher Behandlungskosten Weihnachtsgeschenke für die Enkel kaufen konnte und warum sich Polizist Jochen keine Sorgen mehr über die Kosten beim Zahnarzt machen muss.
+		        </div>
+		        <div class="col-3">
+			        <h1 class="alert.danger">Das Zahnspar-Zentrum</h1>
+			       <p>
+				        Im <span class="brand">ZAHNSPAR-ZENTRUM</span> erhalten Sie die Möglichkeit, Ihre Zahnersatzkosten zu vergleichen und sich unverbindlich über Zahnersatz zu informieren. Dank der kostenfreien Preisvergleiche können Sie die Kosten Ihres Zahnersatzes stark senken und bei Ihrer Eigenzuzahlung bis zu 80% einsparen. Unser Motto lautet: Wir machen Zahnersatz bezahlbar!
+			       </p>
+			       <h2>Wie funktioniert es?</h2>
+			       <p>
+Die preisreduzierte Herstellung in unserem Dentallabor in Hamm und die Kooperation mit internationalen Partnern ermöglicht es uns, die Laborkosten für Zahnersatz dermaßen zu senken, dass sich Ihr Eigenanteil an den Gesamtkosten stark verringert. Unser kostengünstiges Angebot macht es Ihnen möglich, sich ästhetischen und hochwertigen Zahnersatz leisten zu können.
+Neben den Preisvergleichen bieten wir Ihnen auch eine umfassende, persönliche Betreuung und Hilfe bei der Finanzierung von Zahnersatz (Ratenzahlung). Wenn Sie sich über Zahnersatz informieren oder einen Preisvergleich durchführen möchten, besuchen Sie uns in unserem Beratungszentrum in der Hammer Innenstadt. Unsere erfahrenen Berater führen einen Kostenvergleich vor Ort durch oder vereinbaren mit Ihnen einen festen Termin für eine ausführliche Zahnersatz- und Finanzierungsberatung.
+			       </p>
+			       <h2>Was kostet es?</h2>
+			       <p>
+Sowohl die Beratung als auch der Kostenvergleich sind für Sie selbstverständlich kostenfrei und unverbindlich.
+			       </p>
+			       <h2>Was brauche ich?</h2>
+			       <p>
+Jede Art von Zahnersatz ist so individuell wie der Patient der ihn trägt. Die Beratung im <span class="brand">ZAHNSPAR-ZENTRUM</span> ist deshalb auf jeden einzelnen Kunden zugeschnitten. Damit wir Sie optimal beraten können, orientieren wir uns an den Befunden Ihrer Zahnarztpraxis. Die Basis jeder Beratung ist deshalb Ihr persönlicher Heil- und Kostenplan. Dieser enthält alle wichtigen Informationen über den benötigten Zahnersatz.
+			       </p>
+		        </div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="brand-img"></div>
+					<div class="footer">
+						<div class="col-2">
+							<p>
+							Zahnspar-Zentrum Hamm
+							<br/>
+							Bahnhofstr. 11
+							<br/>
+							59065 Hamm
+							</p>
+							<p>
+							<i class="fa fa-phone fa-fw lspace"></i>02381 / 2798266
+							<br/>
+							<i class="fa fa-fax fa-fw lspace"></i>02381 / 2798033
+							<br/>
+							<i class="fa fa-envelope-o fa-fw lspace"></i><a href="mailto:info@zahnspar-zentrum.de">info@zahnspar-zentrum.de</a>
+							</p>
+						</div>
+						<div class="col-2">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2473.935904405029!2d7.809804554224591!3d51.67931627169304!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b9740acdbe5c7d%3A0xd0ac8bd4caf700b3!2sBahnhofstra%C3%9Fe+11%2C+59065+Hamm!5e0!3m2!1sde!2sde!4v1410264033267" width="100%"  frameborder="0" style="border:0"></iframe>
+						</div>
+						<div class="col-2">
+						</div>
+					</div>
+			</div>
+		</div>
+	</body>
+</html>
